c3150efef9560eeb0db3972024b902afd7d148b0
Marcos Lima <48837929+MarcosACLima@users.noreply.github.com>
2022-01-24 17:16:55 -0300

    Acessando o Banco de Dados local com o Room

---
+++ AcessoBD/app/src/main/java/com/example/acessobd/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.acessobd;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void cadastrar(View view) {
+        Intent m = new Intent(this, Cadastro.class);
+        startActivity(m);
+    }
+
+    public void listar(View view) {
+        Intent m = new Intent(this, Listagem.class);
+        startActivity(m);
+    }
+}
